0355f3d16d399303dc1ef56093b58a9934c35c48
diegolarraz <diegolarraz1@gmail.com>
2021-07-21 12:50:59 +0100

    first commit

--- cdn.js
+++ cdn.js
@@ -1,1 +1,23 @@
-console.log("HELOOOOOO IVAAAN");
+(function (window) {
+
+    // declare
+    var myApp = {};
+
+    // your sdk init function
+    myApp.init = function (apiKey, userId) {
+        console.log("CONNECTED TO GS_SDK" + " " + "hello" + userId);
+    };
+
+    myApp.playStarted = function () {
+        alert("THIS GAME HAS STARTED")
+    };
+
+    myApp.playFinished = function () {
+        alert("THIS GAME HAS FINISHED")
+    };
+
+    // define your namespace myApp
+    window.myApp = myApp;
+
+})(window, undefined);
+
